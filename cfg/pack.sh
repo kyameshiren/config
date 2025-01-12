1e8d5fbe0df1d0369f108003a8ffcc21cbf27804
ghost <mrbossstyle@gmail.com>
2025-01-12 05:32:08 -0600

    added package installer

---
+++ cfg/pack.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PACKAGES=(git vim tmux)
+
+function is_installed() {
+    dpkg -l | grep -qw "$1"
+}
+
+function install_package() {
+    echo "Installing $1..."
+    sudo apt install -y "$1"
+}
+
+for package in "${PACKAGES[@]}"; do
+    if is_installed "$package"; then
+        echo "$package already installed!"
+    else
+        install_package "$package"
+    fi
+done
+        
+
+
+
+
+
+
+
+
