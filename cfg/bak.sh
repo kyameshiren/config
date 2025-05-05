a1159422a821759538923f33b5636178414fe30e
kyameshiren <136497967+kyameshiren@users.noreply.github.com>
2025-05-05 17:30:40 -0500

    Update bak.sh

--- cfg/bak.sh
+++ cfg/bak.sh
@@ -6,7 +6,11 @@ backup=/home/${user}/bak
 
 configs=(".bashrc" ".tmux.conf" ".vimrc") 
 
+if [ ! -d ~/bak ]; then
+        mkdir ~/bak
+fi
+
 for i in ${configs[@]}; do
-        cp $input/$i ${backup}/${i}_$(date +%F_%T)
+        cp $input/$i ${backup}/${i}_$(date +%F_%T) 2> /dev/null
 done
 
