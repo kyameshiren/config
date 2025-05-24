7dd4730c3efe7a5ad85a369b9e6e328bcbd7921b
Kyame Shiren <mrbossstyle@gmail.com>
2025-05-23 21:50:15 -0500

    eof got removed from plugins.sh somehow

--- cfg/plugins.sh
+++ cfg/plugins.sh
@@ -8,3 +8,4 @@ fi
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
         curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
         vim +PlugInstall +qall
+fi
