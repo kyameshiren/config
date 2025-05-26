4b5d89440f85302bc27a7991d2577aaf1f5cbe62
Kyame Shiren <mrbossstyle@gmail.com>
2025-05-25 23:07:52 -0500

    changed folders logic to make Desktop and Downloads hidden

--- cfg/folders.sh
+++ cfg/folders.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+userDirsConf="$HOME/.config/user-dirs.dirs"
+
 old=(
         "$HOME/Documents"
         "$HOME/Downloads"
@@ -15,6 +17,9 @@ new=(
         "$HOME/pics"
         "$HOME/music"
         "$HOME/vids"
+        "$HOME/.desktop"
+        "$HOME/.downloads"
+        "$HOME/bak"
 )
 
 
@@ -33,4 +38,26 @@ for dir in "${new[@]}"; do
                 echo "$dir already exists!"
         fi
 done
+
+if [[ -d "$HOME/Desktop" ]]; then
+        mv "$HOME/Desktop" "$HOME/.desktop"
+fi
+
+if [[ -d "$HOME/Downloads" ]]; then
+        mv "$HOME/Downloads" "$HOME/.downloads"
+fi
+
+if [[ -f "$userDirsConf" ]]; then
+        cp "$userDirsConf" "$HOME/bak/$(basename "$userDirsConf").bak"
+
+        sed -i 's|^XDG_DESKTOP_DIR=.*|XDG_DESKTOP_DIR="$HOME/.desktop"|' "$userDirsConf"
+        sed -i 's|^XDG_DOWNLOAD_DIR=.*|XDG_DOWNLOAD_DIR="$HOME/downloads"|' "$userDirsConf"
+        sed -i 's|^XDG_DOCUMENTS_DIR=.*|XDG_DOCUMENTS_DIR="$HOME/files"|' "$userDirsConf"
+        sed -i 's|^XDG_MUSIC_DIR=.*|XDG_MUSIC_DIR="$HOME/music"|' "$userDirsConf"
+        sed -i 's|^XDG_PICTURES_DIR=.*|XDG_PICTURES_DIR="$HOME/pics"|' "$userDirsConf"
+        sed -i 's|^XDG_VIDEOS_DIR=.*|XDG_VIDEOS_DIR="$HOME/vids"|' "$userDirsConf"
+
+fi
+
+
 
